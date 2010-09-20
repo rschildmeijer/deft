258b9c822f23a545a7c584640d8f394403113ab3
Roger Schildmeijer <schildmeijer@gmail.com>
2010-09-20 20:58:27 +0200

    Naive implementation of the http request decoding. Also added a test for the decoding. Added a performance test for http request decoding.

--- src/main/java/org/deft/web/protocol/HttpRequestMessage.java
+++ src/main/java/org/deft/web/protocol/HttpRequestMessage.java
@@ -2,6 +2,9 @@ package org.deft.web.protocol;
 
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,10 +12,36 @@ import org.slf4j.LoggerFactory;
 public class HttpRequestMessage {
 
 	private final static Logger logger = LoggerFactory.getLogger(HttpRequestMessage.class);
-	
-	public static HttpRequestMessage of(ByteBuffer buffer) {
-		logger.debug(new String(buffer.array(), Charset.forName("US-ASCII")));
-		return new HttpRequestMessage();
+
+	private String requestLine;
+	private Map<String, String> headers;
+
+	public HttpRequestMessage(String requestLine, Map<String, String> headers) {
+		this.requestLine = requestLine;
+		this.headers = headers;
 	}
 	
+	public static HttpRequestMessage of(ByteBuffer buffer) {
+		//logger.debug(new String(buffer.array(), Charset.forName("US-ASCII")));
+		String raw = new String(buffer.array(), Charset.forName("US-ASCII"));
+		String[] fields = raw.split("\\r\\n");
+
+		String requestLine = fields[0];
+		Map<String, String> generalHeaders = new HashMap<String, String>();
+		for (int i = 1; i < fields.length; i++) {
+			String[] header = fields[i].split(": ");
+			generalHeaders.put(header[0], header[1]);
+		}
+		return new HttpRequestMessage(requestLine, generalHeaders);
+		
+	}
+
+	public String getRequestLine() {
+		return requestLine;
+	}
+
+	public Map<String, String> getHeaders() {
+		return Collections.unmodifiableMap(headers);
+	}
+
 }
