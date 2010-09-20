4178122d900fc971710ccea6a5c06e3c0e3b5753
jimpetersson <jim.petersson@gmail.com>
2010-09-20 15:30:48 +0200

    Added some debug logging

--- src/main/java/org/deft/web/protocol/HttpProtocolImpl.java
+++ src/main/java/org/deft/web/protocol/HttpProtocolImpl.java
@@ -5,13 +5,18 @@ import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HttpProtocolImpl implements HttpProtocol {
+	
+	private final static Logger logger = LoggerFactory.getLogger(HttpProtocolImpl.class);
 	
 	private static final int BUFFER_SIZE = 512;	//in bytes
 
 	@Override
 	public void handleAccept(SelectionKey key) throws IOException {
+		logger.debug("Received accept event");
 		SocketChannel clientChannel = ((ServerSocketChannel) key.channel()).accept();
 		clientChannel.configureBlocking(false);
 		clientChannel.register(key.selector(), SelectionKey.OP_READ, ByteBuffer.allocate(BUFFER_SIZE));
@@ -19,6 +24,7 @@ public class HttpProtocolImpl implements HttpProtocol {
 
 	@Override
 	public void handleRead(SelectionKey key) throws IOException {
+		logger.debug("Received read event");
 		SocketChannel clientChannel = (SocketChannel) key.channel();
 		ByteBuffer buffer = (ByteBuffer) key.attachment();
 		long bytesRead = clientChannel.read(buffer);
@@ -28,6 +34,7 @@ public class HttpProtocolImpl implements HttpProtocol {
 
 	@Override
 	public void handleWrite(SelectionKey key) throws IOException {
+		logger.debug("Received write event");
 		// TODO Auto-generated method stub
 
 	}
