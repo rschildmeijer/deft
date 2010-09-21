b4111a522ce1a1cdb615c5645876332f1e93d7da
Roger Schildmeijer <schildmeijer@gmail.com>
2010-09-21 20:54:38 +0200

    added trim() to (empty, i.e. "") String candidates in ArrayUtil.removeTrailingEmptyStrings

--- src/main/java/org/deft/web/util/ArrayUtil.java
+++ src/main/java/org/deft/web/util/ArrayUtil.java
@@ -7,7 +7,7 @@ public class ArrayUtil {
 //	private static final String[] EMPTY_STRING_ARRAY = new String[0];
 	public static String[] removeTrailingEmptyStrings(String[] array) {
 		for (int i = array.length - 1 ;i >= 0; i--) {
-			if (!array[i].equals("")) {
+			if (!array[i].trim().equals("")) {
 				String[] trimmedArray = new String[i+1];
 				System.arraycopy(array, 0, trimmedArray, 0, i+1);
 				return trimmedArray; 
