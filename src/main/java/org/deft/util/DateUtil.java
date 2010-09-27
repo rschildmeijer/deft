5d14cd50e0bbf93898c49d3f1186194713c90b39
jimpetersson <jim.petersson@gmail.com>
2010-09-27 21:04:01 +0200

    Fixed bug in DateUtil. Closes #34

--- src/main/java/org/deft/util/DateUtil.java
+++ src/main/java/org/deft/util/DateUtil.java
@@ -2,8 +2,7 @@ package org.deft.util;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
 
@@ -13,17 +12,15 @@ public class DateUtil {
 	private final static TimeZone GMT_ZONE;
 	private final static String RFC_1123_PATTERN = "EEE, dd MMM yyyy HH:mm:ss zzz";
 	private final static DateFormat RFC_1123_FORMAT;
-	private final static Calendar CALENDAR;
 	
 	static {
 		RFC_1123_FORMAT = new SimpleDateFormat(RFC_1123_PATTERN, LOCALE);
 		GMT_ZONE = TimeZone.getTimeZone("GMT");
 		RFC_1123_FORMAT.setTimeZone(GMT_ZONE);
-		CALENDAR = new GregorianCalendar(GMT_ZONE, LOCALE);
 	}
 		
 	public static String getCurrentAsString() {
-		return RFC_1123_FORMAT.format(CALENDAR.getTime());
+		return RFC_1123_FORMAT.format(new Date());
 	}
 	
 }
