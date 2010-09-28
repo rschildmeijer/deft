055f6166a90a3fc6a0675deb1d4bc121569e092f
Roger Schildmeijer <schildmeijer@gmail.com>
2010-09-28 22:31:36 +0200

    Partial #10 (Regular expressions (with capturing groups) in URL mapping). Prepared Application with capturing groups. Also added some tests that verifies the capturing functionality in Application

--- src/main/java/org/deft/web/Application.java
+++ src/main/java/org/deft/web/Application.java
@@ -4,19 +4,47 @@ import java.util.Map;
 
 import org.deft.web.handler.RequestHandler;
 
-
 import com.google.common.collect.ImmutableMap;
 
 public class Application {
 	
 	private final ImmutableMap<String, RequestHandler> handlers;
+	private final ImmutableMap<String, RequestHandler> capturingHandlers;
 
 	public Application(Map<String, RequestHandler> handlers) {
-		this.handlers = ImmutableMap.copyOf(handlers);
+		ImmutableMap.Builder<String, RequestHandler> builder = new ImmutableMap.Builder<String, RequestHandler>();
+		ImmutableMap.Builder<String, RequestHandler> capturingBuilder = new ImmutableMap.Builder<String, RequestHandler>();
+		for (String path : handlers.keySet()) {
+			int index = path.lastIndexOf("/");
+			String group = path.substring(index+1, path.length());
+			if (containsCapturingGroup(group)) {
+				// path ends with capturing group, e.g path == "/person/([0-9]+)"
+				capturingBuilder.put(path.substring(0, index+1), handlers.get(path));
+			} else {
+				// "normal" path, e.g. path == "/"
+				builder.put(path, handlers.get(path));
+			}
+		}
+		this.handlers = builder.build();
+		this.capturingHandlers = capturingBuilder.build();
 	}
 	
 	public RequestHandler getHandler(String path) {
-		return handlers.get(path);
+		RequestHandler rh = handlers.get(path);
+		if (rh == null) {
+			// path could contain capturing groups which we could have a handler associated with.
+			int index = path.lastIndexOf("/");
+			if (index != -1) {
+				String init = path.substring(0, index+1);	// path without its last segment
+				rh = capturingHandlers.get(init);
+			}
+		} 
+		return rh;
+	}
+	
+	private boolean containsCapturingGroup(String group) {
+		return group.matches("^\\(.*\\)$");	// TODO RS 100928 maybe we should verify that the string inside the capturing
+											// is a regular expression. 
 	}
 
 }
--- src/test/java/org/deft/web/ApplicationTest.java
+++ src/test/java/org/deft/web/ApplicationTest.java
@@ -17,14 +17,33 @@ public class ApplicationTest {
 	@Test
 	public void simpleApplicationTest() {
 		Map<String, RequestHandler> handlers = new HashMap<String, RequestHandler>();
-		final RequestHandler handler = new RequestHandler() {
+		final RequestHandler handler1 = new RequestHandler() {
+			@Override public void get(HttpRequest request, HttpResponse response) { }
+		};
+		final RequestHandler handler2 = new RequestHandler() {
+			@Override public void get(HttpRequest request, HttpResponse response) { }
+		};
+		final RequestHandler handler3 = new RequestHandler() {
+			@Override public void get(HttpRequest request, HttpResponse response) { }
+		};
+		final RequestHandler handler4 = new RequestHandler() {
 			@Override public void get(HttpRequest request, HttpResponse response) { }
 		};
 		
-		handlers.put("/", handler);
+		handlers.put("/", handler1);
+		handlers.put("/persons/([0-9]+)", handler2);
+		handlers.put("/persons/phone_numbers", handler3);
+		handlers.put("/pets/([0-9]{0,3})", handler4);
 		Application app = new Application(handlers);
 		
 		assertNotNull(app.getHandler("/"));
-		assertEquals(handler, app.getHandler("/"));
+		assertNotNull(app.getHandler("/persons/1911"));
+		assertNotNull(app.getHandler("/persons/phone_numbers"));
+		assertNotNull(app.getHandler("/pets/123"));
+		
+		assertEquals(handler1, app.getHandler("/"));
+		assertEquals(handler2, app.getHandler("/persons/1911"));
+		assertEquals(handler3, app.getHandler("/persons/phone_numbers"));
+		assertEquals(handler4, app.getHandler("/pets/123"));
 	}
 }
