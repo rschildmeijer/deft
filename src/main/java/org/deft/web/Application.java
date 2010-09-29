03b5ae85ac1a2196c0393ee8df646d439f685153
Roger Schildmeijer <schildmeijer@gmail.com>
2010-09-29 18:18:03 +0200

    Improved group capturing in Application (verifies that the group capturing contains a valid regex. verifies that the last path segment (if it contains a group capturing) in a http request matches the regex given by the client)

--- src/main/java/org/deft/web/Application.java
+++ src/main/java/org/deft/web/Application.java
@@ -1,25 +1,32 @@
 package org.deft.web;
 
 import java.util.Map;
+import java.util.regex.Pattern;
 
 import org.deft.web.handler.RequestHandler;
 
 import com.google.common.collect.ImmutableMap;
 
 public class Application {
-	
+
 	private final ImmutableMap<String, RequestHandler> handlers;
+
 	private final ImmutableMap<String, RequestHandler> capturingHandlers;
+	private final ImmutableMap<RequestHandler, Pattern> regexps;
+
 
 	public Application(Map<String, RequestHandler> handlers) {
 		ImmutableMap.Builder<String, RequestHandler> builder = new ImmutableMap.Builder<String, RequestHandler>();
 		ImmutableMap.Builder<String, RequestHandler> capturingBuilder = new ImmutableMap.Builder<String, RequestHandler>();
+		ImmutableMap.Builder<RequestHandler, Pattern> regexpsBuilder = new ImmutableMap.Builder<RequestHandler, Pattern>();
+
 		for (String path : handlers.keySet()) {
 			int index = path.lastIndexOf("/");
 			String group = path.substring(index+1, path.length());
 			if (containsCapturingGroup(group)) {
 				// path ends with capturing group, e.g path == "/person/([0-9]+)"
 				capturingBuilder.put(path.substring(0, index+1), handlers.get(path));
+				regexpsBuilder.put(handlers.get(path), Pattern.compile(group));
 			} else {
 				// "normal" path, e.g. path == "/"
 				builder.put(path, handlers.get(path));
@@ -27,25 +34,38 @@ public class Application {
 		}
 		this.handlers = builder.build();
 		this.capturingHandlers = capturingBuilder.build();
+		this.regexps = regexpsBuilder.build();
 	}
-	
+
 	public RequestHandler getHandler(String path) {
 		RequestHandler rh = handlers.get(path);
 		if (rh == null) {
 			// path could contain capturing groups which we could have a handler associated with.
-			int index = path.lastIndexOf("/");
-			if (index != -1) {
-				String init = path.substring(0, index+1);	// path without its last segment
-				// TODO RS 100929 verify that the last path segment (the group capturing) matches the regex from the client
-				rh = capturingHandlers.get(init);
-			}
+			rh = getCapturingHandler(path);
 		} 
 		return rh;
 	}
-	
+
 	private boolean containsCapturingGroup(String group) {
-		return group.matches("^\\(.*\\)$");	// TODO RS 100928 maybe we should verify that the string inside the capturing
-											// is a regular expression. 
+		boolean containsGroup =  group.matches("^\\(.*\\)$");
+		Pattern.compile(group);	// throws PatternSyntaxException if group is malformed regular expression
+		return containsGroup;
+	}
+
+	private RequestHandler getCapturingHandler(String path) {
+		int index = path.lastIndexOf("/");
+		if (index != -1) {
+			String init = path.substring(0, index+1);	// path without its last segment
+			String group = path.substring(index+1, path.length()); 
+			RequestHandler handler = capturingHandlers.get(init);
+			if (handler != null) {
+				Pattern regex = regexps.get(handler);
+				if (regex.matcher(group).matches()) {
+					return handler;
+				}
+			}
+		}
+		return null;
 	}
 
 }
--- src/test/java/org/deft/web/ApplicationTest.java
+++ src/test/java/org/deft/web/ApplicationTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertNotNull;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.PatternSyntaxException;
 
 import org.deft.web.handler.RequestHandler;
 import org.deft.web.protocol.HttpRequest;
@@ -41,9 +42,30 @@ public class ApplicationTest {
 		assertNotNull(app.getHandler("/persons/phone_numbers"));
 		assertNotNull(app.getHandler("/pets/123"));
 		
+		assertEquals(null, app.getHandler("/missing"));
+		assertEquals(null, app.getHandler("/persons/"));
+		assertEquals(null, app.getHandler("/persons/roger"));
+		assertEquals(null, app.getHandler("/persons/123a"));
+		assertEquals(null, app.getHandler("/persons/a123"));
+		assertEquals(null, app.getHandler("/pets/a123"));
+		assertEquals(null, app.getHandler("/pets/123a"));
+		assertEquals(null, app.getHandler("/pets/1234"));
+		
 		assertEquals(handler1, app.getHandler("/"));
 		assertEquals(handler2, app.getHandler("/persons/1911"));
 		assertEquals(handler3, app.getHandler("/persons/phone_numbers"));
 		assertEquals(handler4, app.getHandler("/pets/123"));
+	}
+	
+	@Test(expected=PatternSyntaxException.class)
+	public void malFormedRegularExpressionTest() {
+		Map<String, RequestHandler> handlers = new HashMap<String, RequestHandler>();
+		final RequestHandler handler1 = new RequestHandler() {
+			@Override public void get(HttpRequest request, HttpResponse response) { }
+		};
+		
+		handlers.put("/persons/([[0-9]{0,3})", handler1);	// path contains malformed (a '[' too much) regex
+		Application app = new Application(handlers);
+
 	}
 }
